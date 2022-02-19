6fb99ffa62a4faa7b4ad397fb3c90196856b1f37
mimi-mahamah <mimi-mahamah@github.com>
2022-02-19 13:48:18 -0800

    fixed modal popup on page load and refresh

--- index.js
+++ index.js
@@ -29,24 +29,17 @@ const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close]')
 const overlay = document.getElementById('overlay')
 
+window.onload = function() {
+    if (modal == null) return
+    modal.classList.remove('active')
+};
+
+// Open modal function
+
 openModalButtons.forEach(button => {
     button.addEventListener('click', () => {
         const modal = document.querySelector(button.dataset.modalTarget)
         openModal(modal)
-    })
-})
-
-overlay.addEventListener('click', () => {
-    const modals = document.querySelectorAll('.modal.active')
-    modals.forEach(modal => {
-        closeModal(modal)
-    })
-})
-
-closeModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = button.closest('.modal')
-        closeModal(modal)
     })
 })
 
@@ -56,8 +49,23 @@ function openModal(modal) {
     overlay.classList.add('active')
 }
 
+// Close modal function
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    })
+})
+
 function closeModal(modal) {
     if (modal == null) return
     modal.classList.remove('active')
     overlay.classList.remove('active')
 }
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+        closeModal(modal)
+    })
+})
